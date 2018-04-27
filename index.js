d42687d279debe672aa1ab0f2fdf3d9e8463d9b8
frncsdrk <git@frncsdrk.me>
2018-04-27 19:48:10 +0200

    Add example of creating random avatars with squares, vertically symmetric

--- index.js
+++ index.js
@@ -4,6 +4,12 @@ const ctx = canvas.getContext('2d');
 const mkdirp = require('mkdirp');
 const fs = require('fs');
 
+const WIDTH = 256;
+const HEIGHT = 256;
+const squareWidth = 16;
+const squareHeight = 16;
+const symmetric = true;
+
 // Write "Awesome!"
 /*
 ctx.font = '30px Impact';
@@ -19,6 +25,8 @@ ctx.lineTo(50 + text.width, 102);
 ctx.stroke();
 */
 
+// cross with border
+/*
 ctx.strokeStyle = 'rgba(0,0,0,0.5)';
 ctx.lineWidth = 3;
 ctx.beginPath();
@@ -39,6 +47,25 @@ ctx.lineTo(0, 256);
 ctx.lineTo(0, 0);
 
 ctx.stroke();
+*/
+
+ctx.fillStyle = 'blue';
+const elementsPerRow = !symmetric ? WIDTH / squareWidth : WIDTH / squareWidth / 2;
+const elementsPerCol = HEIGHT / squareHeight;
+
+for (let i = 0; i < elementsPerCol; i++) {
+    for (let j = 0; j < elementsPerRow; j++) {
+        if (Math.random() > 0.5) {
+            ctx.rect(j * squareWidth, i * squareHeight, squareWidth, squareHeight);
+            if (symmetric) {
+                // mirror vertically
+                ctx.rect(((elementsPerRow * 2 - j) - 1) * squareWidth, i * squareHeight, squareWidth, squareHeight);
+            }
+        }
+    }
+}
+
+ctx.fill();
 
 const buffer = canvas.toBuffer();
 
