a450b43b6f2d531bf43a452a70e1c397b0d8fa58
frncsdrk <git@frncsdrk.me>
2018-04-30 14:57:24 +0200

    Wrap logic in express microservice
    update readme

--- README.md
+++ README.md
@@ -2,3 +2,19 @@ avatar
 ===
 
 create an avatar
+
+## routes
+
+### /avatar
+
+- GET
+  - query params
+    - width
+    - height
+    - symmetric
+    - type (element type, 'square', 'circle')
+    - noe (number of elements per row and column)
+
+### /status
+
+comming soon
---
+++ src/helpers/avatar.js
@@ -0,0 +1,81 @@
+const { createCanvas, loadImage } = require('canvas');
+const canvas = createCanvas(256, 256);
+const ctx = canvas.getContext('2d');
+const mkdirp = require('mkdirp');
+const fs = require('fs');
+
+const createAvatar = (conf, cb) => {
+    const qs = conf.qs;
+    const res = conf.res;
+
+    const WIDTH = conf.width || 256;
+    const HEIGHT = conf.height || 256;
+    const squareWidth = conf.element_width || 16;
+    const squareHeight = conf.element_height || 16;
+    const symmetric = conf.symmetric || true;
+
+    // Write "Awesome!"
+    /*
+    ctx.font = '30px Impact';
+    ctx.rotate(0.1);
+    ctx.fillText('Awesome!', 50, 100);
+
+    // Draw line under text
+    const text = ctx.measureText('Awesome!');
+    ctx.strokeStyle = 'rgba(0,0,0,0.5)';
+    ctx.beginPath();
+    ctx.lineTo(50, 102);
+    ctx.lineTo(50 + text.width, 102);
+    ctx.stroke();
+    */
+
+    // cross with border
+    /*
+    ctx.strokeStyle = 'rgba(0,0,0,0.5)';
+    ctx.lineWidth = 3;
+    ctx.beginPath();
+
+    // cross
+    ctx.moveTo(0, 0);
+    ctx.lineTo(256, 256);
+
+    ctx.moveTo(256, 0);
+    ctx.lineTo(0, 256);
+
+    // outer border
+    ctx.lineWidth = 5;
+    ctx.moveTo(0, 0);
+    ctx.lineTo(256, 0);
+    ctx.lineTo(256, 256);
+    ctx.lineTo(0, 256);
+    ctx.lineTo(0, 0);
+
+    ctx.stroke();
+    */
+
+    ctx.fillStyle = 'blue';
+    const elementsPerRow = !symmetric ? WIDTH / squareWidth : WIDTH / squareWidth / 2;
+    const elementsPerCol = HEIGHT / squareHeight;
+
+    for (let i = 0; i < elementsPerCol; i++) {
+        for (let j = 0; j < elementsPerRow; j++) {
+            if (Math.random() > 0.5) {
+                ctx.rect(j * squareWidth, i * squareHeight, squareWidth, squareHeight);
+                if (symmetric) {
+                    // mirror vertically
+                    ctx.rect(((elementsPerRow * 2 - j) - 1) * squareWidth, i * squareHeight, squareWidth, squareHeight);
+                }
+            }
+        }
+    }
+
+    ctx.fill();
+
+    const buffer = canvas.toBuffer();
+
+    cb(null, buffer);
+};
+
+module.exports = {
+    createAvatar
+};
