2916c31fa555014938e25ae2c7d885b26be99995
frncsdrk <git@frncsdrk.me>
2021-08-13 00:47:25 +0200

    WIP: Add initials

---
+++ src/utils/initials.js
@@ -0,0 +1,50 @@
+const { parseConfig, createContext } = require('./config')
+
+/**
+ * Draw selected shape with context provided
+ * @param {object} context
+ */
+const draw = (context) => {
+  const ctx = context.ctx
+  ctx.font = context.fontSize + 'px' + ' ' + context.fontFamily // default: '30px Helvetica'
+  // ctx.rotate(0.1)
+  ctx.fillText(context.letters, (context.WIDTH / 2) - (context.fontSize / 2), (context.HEIGHT / 2) - (context.fontSize / 2))
+  // ctx.fillText(context.letters, 50, 100)
+}
+
+const createInitialsAvatar = (conf, cb) => {
+  conf = parseConfig(conf)
+
+  const context = createContext(conf)
+  const ctx = context.ctx
+  const mimeType = conf.mimeType || 'image/png'
+
+  if (context.bgColor) {
+    ctx.beginPath()
+    ctx.fillStyle = conf.bgColor
+    ctx.fillRect(0, 0, context.WIDTH, context.HEIGHT)
+  }
+
+  // NOTE: Need to change colors after bg is drawn
+  ctx.fillStyle = conf.color || 'blue'
+  ctx.strokeStyle = conf.strokeColor || 'black'
+
+  draw(context)
+
+  ctx.fill()
+  context.stroke && ctx.stroke()
+
+  const buffer = context.canvas.toBuffer(mimeType)
+
+  return cb(
+    null,
+    {
+      contentType: mimeType,
+      buffer
+    }
+  )
+}
+
+module.exports = {
+  createInitialsAvatar
+}
