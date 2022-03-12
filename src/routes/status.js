3a4207dc17e8cd2c00aa91a489a81b765a0b5bfb
frncsdrk <git@frncsdrk.me>
2022-03-12 22:10:38 +0100

    feat/api-docs (#11)
    * Add api docs
    * Update readme
    * Add publish script for docs

--- src/routes/status.js
+++ src/routes/status.js
@@ -4,6 +4,26 @@ const respond = require('../utils/respond');
 
 const router = express.Router();
 
+/**
+ * @api {get} /status Request service status
+ * @apiName Status
+ * @apiGroup Health
+ *
+ * @apiVersion 0.1.0
+ * @apiDeprecated use (Health:Healthz)
+ *
+ * @apiSuccess {Boolean} deprecated Deprecated flag
+ * @apiSuccess {String}  message    Message text
+ * @apiSuccess {Integer} status     Status code
+ *
+ * @apiSuccessExample Success-Response:
+ *     HTTP/1.1 200 OK
+ *     {
+ *       "deprecated": true,
+ *       "message": "OK",
+ *       "status": 200
+ *     }
+ */
 router.get('/', (req, res, next) => {
   respond({
     data: {
